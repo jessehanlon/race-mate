8a5807f8f464f05a7614fdaf8c5832463f4b17a1
Jesse <jjhanlon@mun.ca>
2023-04-20 17:57:19 -0230

    added data input for new sailboats

--- racemate/src/App.js
+++ racemate/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import boatData from "./boat_data.json";
 import * as React from 'react';
 import { useTable } from 'react-table';
 
+var boats = [];
+
+
+
 function App() {
-  const data = React.useMemo(() => []);
+  const data = React.useMemo((boats) => []);
   const columns = React.useMemo(() => [
     {
       Header: "Boat",
@@ -58,7 +63,7 @@ function App() {
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({columns, data});
 
   return (
-    <div className='race-results'>
+    <div className='race_results'>
       <div className='container'>
         <table {...getTableProps()}>
           <thead>
@@ -72,13 +77,92 @@ function App() {
               </tr>
             )};
           </thead>
-          <tbody>
-
+          <tbody {...getTableBodyProps() }>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()}>
+                    {cell.render("Cell")}
+                  </td>
+                ))}
+              </tr>
+            )
+          })}
+            
           </tbody>
         </table>
       </div>
+
+      <div id="add-boat">
+        <a id="add-boat-title">Add Boat</a>
+        <div class="input-group" id="boat-details">
+              <span class="input-group-text" id="inputGroup-sizing-sm">Boat Name</span>
+              <input type="text" class="form-control" id="boat" aria-describedby="inputGroup-sizing-sm"></input>
+          </div>
+          <div class="input-group">
+            <span class="input-group-text" id="inputGroup-sizing-sm">Sail Number</span>
+              <input type="text" class="form-control" id="sail_no" aria-describedby="inputGroup-sizing-sm"></input>
+          </div>
+          <div class="input-group">
+            <span class="input-group-text" id="inputGroup-sizing-sm">Boat Design</span>
+              <input type="text" class="form-control" id="design" aria-describedby="inputGroup-sizing-sm"></input>
+          </div>
+          <div class="input-group">
+            <span class="input-group-text" id="inputGroup-sizing-sm">Owner</span>
+              <input type="text" class="form-control" id="owner" aria-describedby="inputGroup-sizing-sm"></input>
+          </div>
+          <div class="input-group">
+            <span class="input-group-text" id="inputGroup-sizing-sm">PHRF Rating</span>
+              <input type="text" class="form-control" id="rating" aria-describedby="inputGroup-sizing-sm"></input>
+          </div>
+          <div class="input-group">
+            <span class="input-group-text" id="inputGroup-sizing-sm">Finish Time</span>
+              <input type="text" class="form-control" id="finish" aria-describedby="inputGroup-sizing-sm"></input>
+          </div>
+          <div class="input-group">
+            <span class="input-group-text" id="inputGroup-sizing-sm">Comments</span>
+              <input type="text" class="form-control" id="comments" aria-describedby="inputGroup-sizing-sm"></input>
+          </div>
+        </div>
+        
+        <div id="add-boat-btn-section">    
+          <button type="button" class="btn btn-outline-primary" id="add-boat-btn">Add Boat</button>                        
+        </div>
+
     </div>
   )
 }
 
+
+/*
+document.getElementById("add-boat-btn").onclick = function() {
+  var boat = document.getElementById("boat").value;
+  var sail_no = document.getElementById("sail_no").value;
+  var design = document.getElementById("design").value;
+  var owner = document.getElementById("owner").value;
+  var rating = document.getElementById("rating").value;
+  var finish = document.getElementById("finish").value;
+  var comments = document.getElementById("comments").value;
+
+  var tcf = 650 / (550 + rating);
+  
+  var newBoat = {
+    "boat":boat,
+    "sail_no":sail_no,
+    "design":design,
+    "owner":owner,
+    "rating":rating,
+    "tcf":tcf,
+    "finish":finish,
+    "comments":comments,
+  }
+  boats.push(newBoat);
+}
+*/
+
 export default App;
+
+
+
